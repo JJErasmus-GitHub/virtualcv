0d8e59788d9d43c97dcde21b488845325d71b5e9
JJErasmus-Github <worsteinerasmus@gmail.com>
2024-08-01 21:51:15 +0200

    fixed pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   app: {
-    baseURL: "JJErasmus-GitHub.github.io",
+    baseURL: "/virtualcv",
   },
 })
